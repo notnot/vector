4219c2777cc35dee37f31f6e87e7837dbd406d8b
Erwin Driessens <snesseird@gmail.com>
2022-06-06 01:29:10 +0200

    NewVec*() i32

--- i32/vec2/vec2.go
+++ i32/vec2/vec2.go
@@ -8,6 +8,10 @@ import (
 
 type Vec2 [2]int32
 
+func NewVec2() *Vec2 {
+	return &Vec2{}
+}
+
 func (v Vec2) String() string {
 	return fmt.Sprintf("(%d, %d)", v[0], v[1])
 }
--- i32/vec3/vec3.go
+++ i32/vec3/vec3.go
@@ -8,6 +8,10 @@ import (
 
 type Vec3 [3]int32
 
+func NewVec3() *Vec3 {
+	return &Vec3{}
+}
+
 func (v Vec3) String() string {
 	return fmt.Sprintf("(%d, %d, %d)", v[0], v[1], v[2])
 }
